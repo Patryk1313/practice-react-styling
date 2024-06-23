33684807b30576ca3087cbe6bca30c00151fc9d1
Patryk <patryk.13@o2.pl>
2024-06-23 21:20:57 +0200

    Done task 04

---
+++ src/components/Tabs/Tab.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { TabButton } from "./Tabs.styled";
+
+const Tab = ({ label, active, disabled, children, onClick }) => {
+    const handleClick = () => {
+        if (!disabled) {
+            onClick(label);
+        }
+    };
+
+    return (
+        <TabButton
+            onClick={handleClick}
+            className={`${active ? "active" : ""} ${
+                disabled ? "disabled" : ""
+            }`}
+            disabled={disabled}
+        >
+            {label}
+        </TabButton>
+    );
+};
+
+export default Tab;
